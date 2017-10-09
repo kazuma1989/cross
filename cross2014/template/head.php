d41d20bf41da4dd30774b1bb5891a072db76a9ab
kazuma1989 <funifuni.1204@gmail.com>
2017-10-09 16:43:21 +0900

    トップページを作成した
    トップページの中身は Page Builder by SiteOrigin で構成している

--- cross2014/template/head.php
+++ cross2014/template/head.php
@@ -16,7 +16,6 @@
 <link rel="stylesheet" href="<?= get_theme_file_uri('css/styles.css') ?>">
 <link rel="stylesheet" href="<?= get_theme_file_uri('css/classes.css') ?>">
 <link rel="stylesheet" href="<?= get_theme_file_uri('style.css') ?>">
-<link rel="stylesheet" href="<?= get_theme_file_uri('css/home.css') ?>">
 
 <!-- jQuery first, then the original scrpit -->
 <script defer src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
