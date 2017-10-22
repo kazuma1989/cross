ff3d6fff09bc52c08bdf1ed3bb45ab16b611b60a
kazuma1989 <funifuni.1204@gmail.com>
2017-10-22 22:21:48 +0900

    config.json から登録したカスタム投稿タイプは、投稿テンプレートを自動で使えるようにした

--- cross/config/load-config.php
+++ cross/config/load-config.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+// 設定ファイルを読み込む
 $config_json_filename = 'config.json';
 
 $parent_theme_json = get_template_directory().DIRECTORY_SEPARATOR.$config_json_filename;
@@ -21,10 +22,29 @@ if ($child_json_exists) {
 
 $config = array_merge_recursive($parent_config, $current_config);
 
+// 読み込んだ内容を使って設定開始
+
 add_action('init', function () use ($config) {
     if (is_array($config['register_post_type'])) {
+        $auto_added_templates_dir = 'template';
+        $auto_added_templates = [];
+        foreach (wp_get_theme()->get_files('php', /* depth */1, /* search_parent */true) as $path => $full_path) {
+            $is_in_template_dir = strpos($path, $auto_added_templates_dir.DIRECTORY_SEPARATOR) === 0;
+            if ($is_in_template_dir) {
+                $auto_added_templates[$path] = str_ireplace([$auto_added_templates_dir.DIRECTORY_SEPARATOR, '.php'], '', $path);
+            } else {
+                continue;
+            }
+        }
+
         foreach ($config['register_post_type'] as $key => $option) {
+            // カスタム投稿タイプの登録
             register_post_type($key, $option);
+
+            // 特定のフォルダー内の PHP ファイルは自動で投稿タイプテンプレートとして使えるようにする
+            add_filter("theme_{$key}_templates", function ($post_templates) use ($auto_added_templates) {
+                return array_merge($auto_added_templates, $post_templates);
+            });
         }
     }
 });
@@ -32,6 +52,7 @@ add_action('init', function () use ($config) {
 add_action('widgets_init', function () use ($config) {
     if (is_array($config['register_sidebar'])) {
         foreach ($config['register_sidebar'] as $option) {
+            // サイドバー（ウィジェットエリア）の登録
             register_sidebar($option);
         }
     }
@@ -39,18 +60,21 @@ add_action('widgets_init', function () use ($config) {
 
 add_action('after_setup_theme', function () use ($config) {
     if (is_array($config['register_nav_menus'])) {
+        // メニューの登録
         register_nav_menus($config['register_nav_menus']);
     }
 
     if (is_array($config['add_theme_support'])) {
         foreach ($config['add_theme_support'] as $key => $option) {
+            // テーマ機能の有効化
             add_theme_support($key, $option);
         }
     }
 });
 
-// Hack
+// hack
 // Wordpress 管理画面で JSON ファイルも編集できるようにする
+// Wordpress バージョンアップにより不要になる可能性が高い
 add_filter('wp_theme_editor_filetypes', function ($default_types) {
     $default_types[] = 'json';
 
