e5013a80ffdb1540997d6dd2d1c76711812eed14
kazuma1989 <funifuni.1204@gmail.com>
2017-10-25 20:42:17 +0900

    WordPress をマルチサイトにした
    wp-config.php が管理されないのは不便なので、環境依存値だけ別ファイルにした。
    wp-config-sample.php は不要になったので削除した。

--- wp-config.php
+++ wp-config.php
@@ -1,6 +1,9 @@
 <?php
-/**
- * The base configuration for WordPress
+
+$config = parse_ini_file('wp-config.env');
+
+/*
+ * The base configuration for WordPress.
  *
  * The wp-config.php creation script uses this file during the
  * installation. You don't have to use the web site, you can
@@ -14,27 +17,25 @@
  * * ABSPATH
  *
  * @link https://codex.wordpress.org/Editing_wp-config.php
- *
- * @package WordPress
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+/* The name of the database for WordPress */
+define('DB_NAME', $config['DB_NAME']);
 
-/** MySQL database username */
-define('DB_USER', 'username_here');
+/* MySQL database username */
+define('DB_USER', $config['DB_USER']);
 
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+/* MySQL database password */
+define('DB_PASSWORD', $config['DB_PASSWORD']);
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+/* MySQL hostname */
+define('DB_HOST', $config['DB_HOST']);
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+/* Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
 
-/** The Database Collate type. Don't change this if in doubt. */
+/* The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
 /**#@+
@@ -46,14 +47,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY', $config['AUTH_KEY']);
+define('SECURE_AUTH_KEY', $config['SECURE_AUTH_KEY']);
+define('LOGGED_IN_KEY', $config['LOGGED_IN_KEY']);
+define('NONCE_KEY', $config['NONCE_KEY']);
+define('AUTH_SALT', $config['AUTH_SALT']);
+define('SECURE_AUTH_SALT', $config['SECURE_AUTH_SALT']);
+define('LOGGED_IN_SALT', $config['LOGGED_IN_SALT']);
+define('NONCE_SALT', $config['NONCE_SALT']);
 
 /**#@-*/
 
@@ -63,9 +64,9 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix = 'wp_';
 
-/**
+/*
  * For developers: WordPress debugging mode.
  *
  * Change this to true to enable the display of notices during development.
@@ -77,13 +78,24 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', $config['WP_DEBUG'] || false);
+define('WP_DEBUG_LOG', $config['WP_DEBUG_LOG'] || false);
+define('WP_DEBUG_DISPLAY', $config['WP_DEBUG_DISPLAY'] || false);
+
+define('WP_ALLOW_MULTISITE', true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', false);
+define('DOMAIN_CURRENT_SITE', $config['DOMAIN_CURRENT_SITE']);
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /* That's all, stop editing! Happy blogging. */
 
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+/* Absolute path to the WordPress directory. */
+if (!defined('ABSPATH')) {
+    define('ABSPATH', dirname(__FILE__).'/');
+}
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+require_once ABSPATH.'wp-settings.php';
