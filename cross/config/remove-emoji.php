151dd645758a62b586b9a5ec8def388d459ff674
kazuma1989 <funifuni.1204@gmail.com>
2017-10-28 19:24:16 +0900

    テーマファイルに持たせていた機能性をできるだけ must use プラグインに寄せた

--- cross/config/remove-emoji.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// 絵文字は使わないので、性能劣化の要因となる補助機能をオフにする
-remove_action('wp_head', 'print_emoji_detection_script', 7);
-remove_action('wp_print_styles', 'print_emoji_styles');
-remove_action('admin_print_scripts', 'print_emoji_detection_script');
-remove_action('admin_print_styles', 'print_emoji_styles');
---
+++ mu-plugins/cross-customize.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: cross 用カスタマイズ設定
+Version:     1.0.0
+Description: 固定ページでカテゴリーを使えるようにする。固定ページ一覧に「テンプレート」の列を表示する。絵文字補助機能をオフにする。SiteOrigin 投稿ループウィジェットで、現在表示中の投稿も表示させる。
+Author:      kazuma1989
+Author URI:  https://github.com/kazuma1989
+*/
+
+declare(strict_types=1);
+
+// 固定ページでカテゴリーを使えるようにする。ただし、カテゴリー検索時に固定ページは対象にはならない
+add_action('init', function () {
+    register_taxonomy_for_object_type('category', 'page');
+});
+
+// 固定ページ一覧に「テンプレート」の列を表示する
+add_filter('manage_pages_columns', function ($posts_columns) {
+    $posts_columns['_wp_page_template'] = 'テンプレート';
+
+    return $posts_columns;
+});
+
+add_action('manage_pages_custom_column', function ($column_name, $post_id) {
+    if ($column_name === '_wp_page_template') {
+        // TODO ファイル名ではなく（ファイル内で定義した）テンプレート名を表示するほうがユーザーフレンドリー
+        echo get_post_meta($post_id, '_wp_page_template', /* single */true);
+    }
+}, 10, 2);
+
+// 絵文字は使わないので、性能劣化の要因となる補助機能をオフにする
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('admin_print_styles', 'print_emoji_styles');
+
+// SiteOrigin 投稿ループウィジェット
+// 現在表示中の投稿と同じ投稿であっても表示させる。
+// 無限ループが生じうるので、投稿ループウィジェットを使うときは利用するテンプレートに注意する。
+add_filter('siteorigin_panels_postloop_query_args', function ($query_args) {
+    unset($query_args['post__not_in']);
+
+    return $query_args;
+});
