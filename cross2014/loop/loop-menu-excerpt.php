2f0c4985f2c0348948305a28d597c9d9703faee1
kazuma1989 <funifuni.1204@gmail.com>
2017-10-29 12:21:52 +0900

    研究テーマ一覧の画面用ループを修整した

--- cross2014/loop/loop-menu-excerpt.php
+++ cross2014/loop/loop-menu-excerpt.php
@@ -2,8 +2,12 @@
     <?php while (have_posts()): the_post() ?>
     <li>
         <a class="a-block" href="<?php the_permalink() ?>">
-            <div class="text-link"><?php the_title() ?></div>
-            <div class="a-description text-small"><?php the_excerpt() ?></div>
+            <div class="text-link">
+                <?php the_title() ?>
+            </div>
+            <div class="a-description text-small">
+                <?php the_excerpt() ?>
+            </div>
         </a>
     </li>
     <?php endwhile ?>
--- cross2014/loop/loop-menu.php
+++
@@ -1,9 +0,0 @@
-<ul class="text-small ul-separate-half">
-    <?php while (have_posts()): the_post() ?>
-    <li>
-        <a href="<?php the_permalink() ?>">
-            <?php the_title() ?>
-        </a>
-    </li>
-    <?php endwhile ?>
-</ul>
