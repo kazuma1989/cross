6d2d4707f622019a5dc14f4e23bf7218ce943ecc
kazuma1989 <funifuni.1204@gmail.com>
2017-11-12 11:22:28 +0900

    環境変数の仕組みを整理した
    env ファイルのないところに再構築するのが大変だったので。
    ついでに WordPress のバージョンを最新にした。
    composer をタスクランナーとして導入した。

--- html/wp-config.php
+++ html/wp-config.php
@@ -18,13 +18,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /* The name of the database for WordPress */
-define('DB_NAME', getenv('DB_NAME'));
+define('DB_NAME', getenv('MYSQL_DATABASE'));
 
 /* MySQL database username */
-define('DB_USER', getenv('DB_USER'));
+define('DB_USER', getenv('MYSQL_USER'));
 
 /* MySQL database password */
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_PASSWORD', getenv('MYSQL_PASSWORD'));
 
 /* MySQL hostname */
 define('DB_HOST', getenv('DB_HOST'));
